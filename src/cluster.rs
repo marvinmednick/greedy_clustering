0786a8530e728add2d58020cf3ebc732e4b73a8a
Marvin Mednick <mmednick@gmail.com>
2022-07-11 14:28:21 -0700

    Improved commenting in cluster about the algorithm

--- src/cluster.rs
+++ src/cluster.rs
@@ -66,7 +66,16 @@ impl ClusteringInfo {
         }
     }
 
+    /* cluster -- main entry point for dividing the entry points into k clusters.
+     * Overall it will follow the same path as kruskals algorithm for MST
+     * by attempting to the smallest avaialble edge that isn't already in the same 
+     * cluster (Analagous to not taking an edge that creates a cycle when building an MST
+     * The code does make use of a Union-Find data structure to allow the lookup of whether
+     * an vertex is already in the same cluster.)
+     */
     pub fn cluster(&mut self, k : usize) -> i32 {
+        // edges are sorted so we will address them in increasing order
+        // this allows us to always take the smallest avaiable edge
         self.edges.sort();
 
         for edge in &self.edges {
@@ -74,17 +83,31 @@ impl ClusteringInfo {
         }
         let mut cur_distance = 0;
         let num_edges = self.edges.len();
+
+        // loop through each of the edges (which are in increasing weight) 
+        // and add the smallest edge to its appropriate cluster
         for index in 0..num_edges {
+            // since the edges are sorted in increasing order, then
+            // the distance to the cluster will be the length of the current edge
             cur_distance = self.edges[index].weight.clone();
             debug!("#{} groups {} dist {} ",index, self.groups, cur_distance);
+            // find the two vertexes at the ends this edge (start, end)
             let start = self.edges[index].start_id.clone();
             let end = self.edges[index].end_id.clone();
+
+            //provided we haven't already reached our desired number of clusters, 
+            // try to combine the start and end vertexes into the same cluster
+            // (union will detect if they are already in the same cluster)
             if self.groups > k {
                 self.union(start, end);
             }
-            // reached the desireed number of groups
-            // so continu processing any reamining edges which are within the same groups
-            // in order to find the correct distance
+            // we've reached the desireed number of groups and we have the next edge
+            // which is potentialy the mininum distance between two clusters -- assuming
+            // that edge is between two vertexes in different clusters -- however since 
+            // we haven't checked it yet, both ends of this edge could be in the same cluster
+            // and then isn't a valid distance between two clusters.   
+            // So we need to to continue sarching through edges until we find the first one
+            // where the two vertexes are in differnt clusters
             else if !self.same_group(start,end) {
                 break
             }
